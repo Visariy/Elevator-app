5d40c226bff0094affb1d72ef13e8df2fac1352e
Visariy <73362241+Visariy@users.noreply.github.com>
2023-04-05 10:42:40 +0300

    Update state.ts

--- src/store/state.ts
+++ src/store/state.ts
@@ -4,9 +4,9 @@ import { useLocalStorage } from '@vueuse/core';
 export const useElevatorState = defineStore('elevatorState', () => {
   const queue = useLocalStorage('queue', [] as number []);
   const transform = useLocalStorage('transform', 0);
-  const currentFloor = useLocalStorage('currentLevel', 1);
-  const preventFloor = useLocalStorage('preventLevel', 0);
-  const moveDirection = useLocalStorage('direction', 'stop');
+  const currentFloor = useLocalStorage('currentFloor', 1);
+  const preventFloor = useLocalStorage('preventFloor', 0);
+  const moveDirection = useLocalStorage('moveDirection', 'stop');
   const transitionDuration = useLocalStorage('duration', 1);
 
   return {
